dae5e18d15dad116f0dcc04b92ab4461fd5170b7
Shaun Karran <shaun.karran@gmail.com>
2015-12-12 14:23:54 +1000

    Changes some test values.

--- test/gl2d_test.c
+++ test/gl2d_test.c
@@ -25,39 +25,39 @@ int main(void) {
 
     cpu_set_speed(CPU_32MHz);
 
-    LED_PORT.DIRSET = LED0 | LED1;
+    LED_PORT.DIRSET = LED0;
 
     lcd5110_init(LCD5110_CONTRAST_MID, &PORTC, LCD_DC, LCD_RST, LCD_SCE);
     gl2d_init(LCD5110_WIDTH, LCD5110_HEIGHT, lcd5110_write_array);
     gl2d_orthographic(0, LCD5110_WIDTH, 0, LCD5110_HEIGHT);
 
     /* Each pair of values defines a vertex in the square. */
-    vertex_array[0] = 0;
-    vertex_array[1] = 0;
-    vertex_array[2] = 1;
-    vertex_array[3] = 0;
-    vertex_array[4] = 1;
-    vertex_array[5] = 1;
-    vertex_array[6] = 0;
-    vertex_array[7] = 1;
-    vertex_array[8] = 0;
-    vertex_array[9] = 0;
+    vertex_array[0] = 37;
+    vertex_array[1] = 19;
+    vertex_array[2] = 47;
+    vertex_array[3] = 19;
+    vertex_array[4] = 47;
+    vertex_array[5] = 29;
+    vertex_array[6] = 37;
+    vertex_array[7] = 29;
+    vertex_array[8] = 37;
+    vertex_array[9] = 19;
 
     char i = 0;
     while (1) {
         gl2d_clear_buffer();
 
         ml_mat3_identity(&mv_matrix);
-        mv_matrix.values[0][2] = i; // Move i in x direction.
-        mv_matrix.values[1][2] = i; // Move i in y direction.
-        i++;
+        // mv_matrix.values[0][2] = i; // Move i in x direction.
+        // mv_matrix.values[1][2] = i; // Move i in y direction.
+        // i++;
 
         gl2d_bind_vertex_array(vertex_array, NUM_VERTICES * 2);
         gl2d_bind_mvmatrix(&mv_matrix);
 
         gl2d_draw(NUM_VERTICES);
 
-        LED_PORT.OUTTGL = LED0 | LED1;
+        LED_PORT.OUTTGL = LED0;
         _delay_ms(100);
     }
 
